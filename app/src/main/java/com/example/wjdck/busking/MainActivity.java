6346d971e903ad4c52031874ae6cb3c05357367b
KwanWoo Kim <rlarhksdn777@naver.com>
2018-07-30 17:12:54 +0900

    yuni'ssssss

--- app/src/main/java/com/example/wjdck/busking/MainActivity.java
+++ app/src/main/java/com/example/wjdck/busking/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //yuni
-        //Project yunieeeeee
+        //Project yunieeeeee323141323313221
     }
 }
